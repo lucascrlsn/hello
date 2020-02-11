985d6d05dd9d797c7bc2366f03974c5dbebdc43a
Lucas Carlson <lucascrlsn@outlook.com>
2020-02-11 15:44:52 -0600

    threading practice
    Here’s a quick threading .py I put together while following along with Corey Schafer. I incorporated the speedtest module to print out the download 10 seconds. The plan is to push speedtest results and os mod outputs all as system logs on a google spreadsheet using gspread. I have already managed to len(spreadsheet) rows and add logs as needed. 
    
    One of the errors I can’t seem to figure out is that the second test executed under the else section produces a number with zero decimal points despite using round. This is supposed to be two decimals so I can return an accurate difference between the two.  I don’t really need that function but it would at least be good to know why it’s doing that. 
    
    Also, at the end of each if/else I get “none” printed out.  
    
    Any thoughts on these errors? Will take a break and check it out later, probably something minor. Thanks all!
    
    His video as well as others I have been following can be found here: https://github.com/lucascrlsn/hello. The video discusses the difference between CPU and IO bound tasks and goes over the strengths/weaknesses and threading and multiprocessing.

---
+++ Threading/threading_practice.py
@@ -0,0 +1,76 @@
+import concurrent.futures
+import time
+import speedtest
+from colorama import Fore, Style, init
+init()
+
+start = time.perf_counter()
+session_count = 0
+session_limit = 2
+last_test = int()
+download_capacity = int()
+
+
+def check_speed(seconds):
+    # Checks Upload/Download Speeds
+    global download_capacity
+    global session_count
+    global last_test
+    if session_count == 0:
+        print('No previous sessions detected, measuring download speed...')
+        speedtester = speedtest.Speedtest()
+        speedtester.get_best_server()
+        speedtester.upload(pre_allocate=False)
+        download_bytes = round(speedtester.download(), 2)
+        download_capacity = round(download_bytes / 1048576, 2)
+        last_test = round(download_bytes / 1048576, 2)
+        print(Fore.RED + f'Download speed: {download_capacity}MB per second' + Style.RESET_ALL)
+        session_count = session_count + 1
+        print(f'Sleeping {seconds} second(s)...')
+        time.sleep(seconds)
+    else:
+        print('Initiating...')
+        speedtester = speedtest.Speedtest()
+        speedtester.get_best_server()
+        speedtester.upload(pre_allocate=False)
+        download_bytes = round(speedtester.download(), 2)
+        download_capacity = int(download_bytes / 1048576)
+        download_capacity = round(download_capacity, 2)
+        difference = round(abs(download_capacity-last_test), 2)
+        print(Fore.RED + f'Download speed: {download_capacity}MB per second, this is a difference of {difference}MB per second' + Style.RESET_ALL)
+        last_test = round(download_capacity, 2)
+        session_count = session_count + 1
+        print(f'Sleeping {seconds} second(s)...')
+        time.sleep(seconds)
+
+
+def run_thread():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Live Version will be set to 120 seconds
+        f1 = executor.submit(check_speed, 10)
+        print(f1.result())
+        global session_limit
+        if session_count < session_limit:
+            run_thread()
+        else:
+            end_session()
+        # prompt = input('Would you like to check the speed again (y/n): ')
+        # if prompt == 'y':
+        #     run_thread()
+        # else:
+        #     end_session()
+
+
+finish = time.perf_counter()
+
+
+def end_session():
+    # Completely ends the program, user will not be passed to additional checks in the active session
+    url = 'https://github.com/lucascrlsn'
+    print(f'You have met the session limit count! Please follow me on Github: {url}')
+    print('Have a good day!')
+    print(f'Finished in {round(finish - start, 2)} second(s)')
+
+
+run_thread()
+
