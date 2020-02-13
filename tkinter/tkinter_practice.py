d9d1ffc8fcdeb0f5b02a873f83739b0e4c25429b
Lucas Carlson <lucascrlsn@outlook.com>
2020-02-13 12:14:33 -0600

    formatting

--- tkinter/tkinter_practice.py
+++ tkinter/tkinter_practice.py
@@ -1,52 +1,53 @@
-#TTK first example
-#traditional tkinter for basis
- 
 from tkinter import *
 #from tkinter import ttk
+
+# https://makeapppie.com/2014/05/04/from-apple-to-raspberry-pi-a-gentle-introduction-to-themed-widgets-with-ttk/
  
-#make the window
-root=Tk()
+
+root = Tk()
 root.title('TTK Power Panel #1')
  
-#make the frame
- 
+# Frame
 frame = Frame(root)
 frame.grid(row=0, column=0)
-#variables
-status =StringVar()
+
+# Variables
+status = StringVar()
  
-#Styles
+# Styles
  
-#controller
+# controller
+
+
 def onButtonPressed():
     status.set('On')
+
+
 def offButtonPressed():
     status.set('Off')
- 
-#view
- 
-#label
+
+
+# label
 status.set('System Off')
-status_label = Label(frame, textvariable = status)
-status_label.configure(font=('Sans','14','bold'),background = 'blue', foreground = '#eeeeff')
-status_label.grid(row=0,column=0,columnspan=3,sticky=EW)
-#on Button
-on_button = Button(frame, command = onButtonPressed, text = 'On')
+status_label = Label(frame, textvariable=status)
+status_label.configure(font=('Sans', '14', 'bold'), background='blue', foreground='#eeeeff')
+status_label.grid(row=0, column=0, columnspan=3, sticky=EW)
+
+# on Button
+on_button = Button(frame, command=onButtonPressed, text='On')
+on_button.configure(font=('Sans', '14', 'bold'),background='blue', foreground='#eeeeff')
+on_button.grid(row=2, column=0)
  
-on_button.configure(font=('Sans','14','bold'),background = 'blue', foreground = '#eeeeff')
-on_button.grid(row = 2, column = 0)
+# off Button
+off_button = Button(frame, command=offButtonPressed, text='Off')
  
-#off Button
-off_button = Button(frame, command = offButtonPressed, text = 'Off')
+off_button.configure(font=('Sans', '14', 'bold'), background='blue', foreground='#eeeeff')
+off_button.grid(row=2, column=1)
  
-off_button.configure(font=('Sans','14','bold'),background = 'blue', foreground = '#eeeeff')
-off_button.grid(row = 2, column = 1)
+quit_button = Button(frame, command=quit, text='Quit')
  
-quit_button = Button(frame, command = quit, text = 'Quit')
- 
-quit_button.configure(font=('Sans','14','bold'),background = 'blue', foreground = '#eeeeff')
-quit_button.grid(row = 2, column = 2)
- 
-#main loop
+quit_button.configure(font=('Sans', '14', 'bold'), background='blue', foreground='#eeeeff')
+quit_button.grid(row=2, column=2)
+
  
 mainloop()
